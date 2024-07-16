088922bae1d4a1666a2cbf14682dc070b8941553
cjb543 <cjb543@protonmail.com>
2024-07-15 23:18:56 -0500

    Add more FEN game state parameters (does not compile)

--- visualizer.c
+++ visualizer.c
@@ -2,22 +2,29 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-void processFEN(char FENString[], char finishedBoard[]);
-void printBoard(char FENString[]);
+void processBoardAppearance(char FENString[], char finishedBoard[]);
+void processGameState(char FENString[], int *whitePieceCount,
+                      int *blackCountPiece, bool *whiteToMove);
+void printBoard(char FENString[], int whitePieceCount, int blackPieceCount,
+                bool whiteToMove);
 
 int main() {
+  int whitePieceCount = 0, blackPieceCount = 0;
+  bool whiteToMove;
   char FENString[91];
   char finishedBoard[65];
 
   printf("Enter a FEN string: ");
   scanf("%s", FENString);
 
-  processFEN(FENString, finishedBoard);
-  printBoard(finishedBoard);
+  processBoardAppearance(FENString, finishedBoard);
+  processGameState(FENString, &whitePieceCount, &blackPieceCount, &whiteToMove);
+  printBoard(finishedBoard, whitePieceCount, blackPieceCount, whiteToMove);
+
   return 0;
 }
 
-void processFEN(char FENString[], char finishedBoard[]) {
+void processBoardAppearance(char FENString[], char finishedBoard[]) {
   int i = 0, j = 0;
   while (FENString[i] != '\0') {
     if (isdigit(FENString[i])) {
@@ -38,7 +45,27 @@ void processFEN(char FENString[], char finishedBoard[]) {
   finishedBoard[j] = '\0';
 }
 
-void printBoard(char finishedBoard[]) {
+void processGameState(char FENString[], int *whitePieceCount,
+                      int *blackPieceCount, bool *whiteToMove) {
+  for (int i = 0; i < 91; i++) {
+    if (isupper(FENString[i]) && isalpha(FENString[i])) {
+      whitePieceCount++;
+    } else if (!isupper(FENString[i]) && isalpha(FENString[i])) {
+      blackPieceCount++;
+    }
+  }
+}
+
+void printBoard(char finishedBoard[], int whitePieceCount, int blackPieceCount,
+                bool whiteToMove) {
   printf("%s", finishedBoard);
   printf("\n");
+
+  printf("White has %d pieces \n", whitePieceCount);
+  printf("Black has %d pieces \n", blackPieceCount);
+
+  if (whiteToMove)
+    printf("White to move \n");
+  else
+    printf("blackToMove \n");
 }
